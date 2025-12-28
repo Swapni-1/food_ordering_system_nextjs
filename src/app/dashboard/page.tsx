1aa4bb3fe0a25a1cd25e3d07dbe1c484a30fa8e8
Swapni-1 <swapnillakra822@gmail.com>
2025-12-28 13:50:32 +0530

    add forced dynamic to prevent static cache

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import {auth} from "@/auth"
 import Auth from "../_components/Authentication";
 import {redirect} from "next/navigation"
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home(){
     const session = await auth();
     
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@
 import { auth } from "@/auth";
 import Auth from "./_components/Authentication";
 import Home from "./Home";
-import dynamic from "next/dynamic";
 
 // const Auth = dynamic(() => import("./_components/Authentication"),{ssr : false})
 
+export const dynamic = 'force-dynamic';
 
 export default async function page(){
   const session = await auth();
