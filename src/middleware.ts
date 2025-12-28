946da82922aa0d5cfd630ab61f5a491d2f1ffd80
Swapni-1 <swapnillakra822@gmail.com>
2025-12-28 12:27:14 +0530

    fixed untrusted host error and not-found page error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,15 @@
 export { auth as middleware } from "@/auth"
+
+export const config = {
+  // This matcher prevents the middleware from running on internal files and static assets
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
